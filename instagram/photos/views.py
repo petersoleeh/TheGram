8bdd06e60cbcd2d2aea7c276a1ee229c3716a9c9
peter soleeh <petersoleeh@gmail.com>
2017-11-26 12:31:06 +0300

    add the profile url and views

--- instagram/photos/views.py
+++ instagram/photos/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Post
 
 # Create your views here
+@login_required(login_url='/accounts/login/')
 def index(request):
     return render(request,'index.html')
+
+
+def profile(request):
+    post = Post.objects.all()
+    return render(request,'all-photos/profile.html',{"post":post})
