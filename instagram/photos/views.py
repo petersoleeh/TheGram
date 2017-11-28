c4bf4880c9282110b008423f8e17e396feab440c
peter soleeh <petersoleeh@gmail.com>
2017-11-28 18:31:58 +0300

    query data from db to display in the profile page

--- instagram/photos/views.py
+++ instagram/photos/views.py
@@ -1,13 +1,43 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Post
+from .forms import NewPostForm
+
+
 
 # Create your views here
 @login_required(login_url='/accounts/login/')
 def index(request):
-    return render(request,'index.html')
+    post = Post.objects.all()
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+
+    else:
+        form = NewPostForm()
+
+    return render(request,'index.html',{"form":form,"post":post})
 
 
 def profile(request):
     post = Post.objects.all()
     return render(request,'all-photos/profile.html',{"post":post})
+
+
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+
+    else:
+        form = NewPostForm()
+
+    return render(request,'all-photos/new_post.html',{"form":form})
